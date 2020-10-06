2dc261599faacc02107ac354eab7215ce898b791
Celia Navarro Pérez <celiinavarroperez@gmail.com>
2020-10-06 17:21:19 +0100

    function initState()

--- lib/src/pages/listen.dart
+++ lib/src/pages/listen.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fft/flutter_fft.dart';
 
 class ListenPage extends StatefulWidget {
-  const ListenPage({Key key}) : super(key: key);
-
   @override
   _ListenPageState createState() => _ListenPageState();
 }
 
 class _ListenPageState extends State<ListenPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Mis citas')),
-        backgroundColor: Colors.indigoAccent,
-      ),
-    );
-  }
+double frequency;
+bool isRecording;
+
+FlutterFft flutterFft = new FlutterFft();
+
+@override
+  void initState() {
+    isRecording = flutterFft.getIsRecording;
+    frequency = flutterFft.getFrequency;
+    super.initState();
+    _initialize();
+      }
+    
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('Mis citas')),
+            backgroundColor: Colors.indigoAccent,
+          ),
+        );
+      }
+    
+      void _initialize() async {
+        
+      }
 }
