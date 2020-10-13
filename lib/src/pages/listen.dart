af22150b74e902c5525d1ca60c3bc777004192c4
Celia Navarro Pérez <celiinavarroperez@gmail.com>
2020-10-13 18:54:22 +0100

    Change text to alert and some style changes

--- lib/src/pages/listen.dart
+++ lib/src/pages/listen.dart
@@ -25,20 +25,48 @@ class _ListenPageState extends State<ListenPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Mi turno')),
+        title: Text('Mi turno'),
         backgroundColor: Colors.indigoAccent,
+        actions: [
+          Container(
+            padding: EdgeInsets.all(8),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i0.wp.com/www.insights.la/wp-content/uploads/2015/12/escuchar-a-tu-cliente-1.png?ssl=1'),
+              radius: 20,
+            ),
+          )
+        ],
       ),
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              FadeInImage(
+                placeholder: AssetImage('assets/listen-gif.gif') ,
+                image: NetworkImage('https://media1.giphy.com/media/eNM4NlGpmCxzcXesjr/giphy.gif')
+               ),
               isRecording && frequency > 1500.00
                   ? FutureBuilder(
                       future: Vibration.vibrate(duration: 2000),
                       builder: (context, AsyncSnapshot snapshot) {
-                        return Text(
-                          "Ha cambiado el turno, por favor verifica si es el tuyo",
-                          style: TextStyle(fontSize: 25),
+                        return AlertDialog(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          title: Text('Cambio de turno'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text(
+                                  'Ha cambiado el turno, por favor verifica si es el tuyo.'),
+                              FlutterLogo(size: 100)
+                            ],
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                                child: Text('OK'),
+                                onPressed: () => Navigator.of(context).pop())
+                          ],
                         );
                       },
                     )
